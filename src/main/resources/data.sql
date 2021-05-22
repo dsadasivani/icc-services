fb4d9f7a545edf25d0958f8cf2ddf17c996cc45c
Dilip Kumar Patnaik Sadasivani <dsadasivani@gmail.com>
2021-05-22 17:55:56 +0530

    Products Initial load
    Pre-load data into products table at the time of server startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE icc_products;
+INSERT INTO icc_products (prod_id,hsn_code,product_desc,product_id) VALUES
+	 (1,'38244010','LEAKSEAL - 1 Ltr x 20 Btls','product1'),
+	 (2,'38244010','LEAKSEAL - 5 Ltr x 4 Jars','product2'),
+	 (3,'38244010','LEAKSEAL - 100 Ml x 100 Pouches','product3');
